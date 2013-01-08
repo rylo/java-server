eebf8030ec05366522450826348acb7aec9d3f94
Ryan Verner <ryanzverner@gmail.com>
2013-01-08 10:57:44 -0600

    adding main method to start up server

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Java server I'm writing as part of my 8th Light apprenticeship. To start the server, run `java Main`.
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import server.Starter;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        new Starter().go();
+    }
+
+}
