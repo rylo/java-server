7091cbac0c43bce49e6cb20f2e2d75bb076d6037
Ryan Verner <ryanzverner@gmail.com>
2013-01-11 11:15:29 -0600

    fixing echo response to handle multiple key value pairs in query string

--- src/server/responses/Echo.java
+++ src/server/responses/Echo.java
@@ -4,11 +4,23 @@ public class Echo {
 
     public String get(String route) {
         String params[] = route.split("\\?");
+        int length = params.length;
+        if(length == 1) {
+            return routeSplitAtAnd(params[0], length);
+        } else {
+            return routeSplitAtAnd(params[1], length);
+        }
+    }
+
+    public String routeSplitAtAnd(String route, int length) {
+        String params[] = route.split("\\&");
         int n;
         String splitRoutes = "Query string parameters are: \n";
-        for(n=1; n < params.length; n++) {
-            splitRoutes += params[n];
-            splitRoutes += "\n";
+        for(n=0; n < params.length; n++) {
+            if(length > 1) {
+                splitRoutes += params[n];
+                splitRoutes += "\n";
+            }
         }
         return splitRoutes;
     }
--- src/specs/EchoTest.java
+++ src/specs/EchoTest.java
@@ -14,9 +14,21 @@ public class EchoTest {
     }
 
     @Test
-    public void canParseMultipleKeyValuePairsInQueryParams() {
+    public void canParseTwoKeyValuePairsInQueryParams() {
         String parsedString = new Echo().get("echo?abc=123&def=456");
         assertEquals(parsedString, "Query string parameters are: \nabc=123\ndef=456\n");
+    }
+
+    @Test
+    public void canParseFiveKeyValuePairsInQueryParams() {
+        String parsedString = new Echo().get("echo?abc=123&def=456&ghi=789&jkl=101&mno=999");
+        assertEquals(parsedString, "Query string parameters are: \nabc=123\ndef=456\nghi=789\njkl=101\nmno=999\n");
+    }
+
+    @Test
+    public void canHandleNoQueryString() {
+        String parsedString = new Echo().get("echo");
+        assertEquals(parsedString, "Query string parameters are: \n");
     }
 
 }
