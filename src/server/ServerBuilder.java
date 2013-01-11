8f7ccfc46e196415e3574f27e11e44b8310d656b
Ryan Verner <ryanzverner@gmail.com>
2013-01-11 08:46:26 -0600

    significant refactoring of class that builds the server

--- src/server/ServerBuilder.java
+++ src/server/ServerBuilder.java
@@ -1,82 +1,29 @@
 package server;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ServerBuilder implements Runnable {
-    protected Socket clientSocket;
-    private ServerSocket serverSocket;
-    public volatile int requests;
-    private int limit = 100;
+public class ServerBuilder {
+    public ServerSocket serverSocket;
+    public volatile int requests = 0;
+    public int limit = 5;
 
-
-    private MyServerSocketGenerator serverSocketGenerator;
-
-    public void start() {
-        serverSocketGenerator.listen();
+    public ServerBuilder(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
     }
 
-    public void setServerSocketGenerator(MyServerSocketGenerator anyServerSocketGenerator) {
-        this.serverSocketGenerator = anyServerSocketGenerator;
-    }
-
-
-
-    public void beginProcess() {
-
-        new Thread(this).start();
-    }
-
-    public void setServerSocket(ServerSocket anyServerSocket) {
-        this.serverSocket = anyServerSocket;
-    }
-
-    public void setRequestParser(MyRequestParser anyRequestParser) {
-        this.requestParser = anyRequestParser;
-    }
-
-    public void setRequestLimit(int anyLimit) {
-        this.limit = anyLimit;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Override
-    public void run() {
+    public void begin() {
         while(requests < limit) {
             try {
-                clientSocket = serverSocket.accept();
-//                requestPaer.beginProcess();
-
-                // you create new Threads within here
-                OutputStream binaryWriter = clientSocket.getOutputStream();
-                PrintWriter stringWriter = new PrintWriter(binaryWriter, true);
-                stringWriter.println("Hello world");
+                Socket clientSocket = serverSocket.accept();
+                ThreadBuilder threadBuilder = new ThreadBuilder(clientSocket);
+                threadBuilder.run();
                 requests++;
-                binaryWriter.close();
                 clientSocket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-//            } catch (ClassNotFoundException e) {
-//                e.printStackTrace();
-//            }
         }
     }
 
