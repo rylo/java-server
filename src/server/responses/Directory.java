387c90ecd1e01084c1073e0baac642230da8ef05
Ryan Verner <ryanzverner@gmail.com>
2013-01-11 14:48:02 -0600

    adding additional logic to directory that has been pulled from response builder

--- src/server/responses/Directory.java
+++ src/server/responses/Directory.java
@@ -1,8 +1,14 @@
 package server.responses;
 
 import java.io.File;
+import java.util.HashMap;
 
 public class Directory {
+    private final HashMap<String, String> httpRequestParameters;
+
+    public Directory(HashMap<String, String> httpRequestParameters) {
+        this.httpRequestParameters = httpRequestParameters;
+    }
 
     public String getResponse(File requestedPath) {
         File[] listOfFiles = getDirectoryContents(requestedPath);
@@ -23,5 +29,32 @@ public class Directory {
         return response;
     }
 
+    public String getDirectoryResponse(String route) {
+        String params;
+        String responseParameters;
+        if(routeIsDirectory(route)) {
+            File requestedDirectory = getRequestedDirectory(route);
+            params = getResponse(requestedDirectory);
+            responseParameters = getResponseParameters(" 200 OK", "text/html");
+        } else {
+            params = "NOT A DIRECTORY";
+            responseParameters = getResponseParameters(" 404 Not Found", "text/html");
+        }
+        return responseParameters + params;
+    }
+
+    public File getRequestedDirectory(String route) {
+        String requestedPath = System.getProperty("user.dir") + "/" + route;
+        return new File(requestedPath);
+    }
+
+    public boolean routeIsDirectory(String route) {
+        File requestedDirectory = getRequestedDirectory(route);
+        return requestedDirectory.isDirectory();
+    }
+
+    public String getResponseParameters(String statusCode, String contentType) {
+        return httpRequestParameters.get("protocol") + statusCode + "\r\n" + "Content-Type: " + contentType + "; charset=UTF-8\r\n\r\n";
+    }
 
 }
